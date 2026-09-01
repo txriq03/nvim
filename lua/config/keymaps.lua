9ff5a5e8bf33002a9f53d3bff3a1563aba8afc21
Tariq Brown <tariq.brown@triadcreative.co.uk>
2026-09-01 10:27:20 +0100

    update keymaps for laravel and lazy

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>l", "<Nop>", { desc = "+laravel" })
+vim.keymap.set("n", "<leader>L", "<cmd>Lazy<cr>", { desc = "Lazy" })
