c12e8d468c4ca7b9b37def0fb51f3f95dde6e5b5
txriq03 <taqbro@gmail.com>
2026-09-17 09:22:49 +0100

    include hidden files in snacks picker and explorer

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        files = { hidden = true },
+        explorer = { hidden = true },
+      },
+    },
+  },
+}
