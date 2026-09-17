ac1bf6111657c2f95f72b128859726a1c6948f8f
txriq03 <taqbro@gmail.com>
2026-09-17 09:17:39 +0100

    Change style of lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,9 +2,12 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
-      vim.schedule(function()
-        vim.opt.laststatus = 0
-      end)
+      opts.options = opts.options or {}
+      opts.options.component_separators = { left = "│", right = "│" }
+      opts.options.section_separators = { left = " ", right = ""   }
+      -- vim.schedule(function()
+      --   vim.opt.laststatus = 0
+      -- end)
 
       return opts
     end,
